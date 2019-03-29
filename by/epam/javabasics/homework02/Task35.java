8fa4fa870db5db5663f0ec36a83450df2eb3b88a
olegk <olegk@DESKTOP-7844GVK>
2019-03-29 15:31:42 +0300

    35,36

--- by/epam/javabasics/homework02/Task35.java
+++ by/epam/javabasics/homework02/Task35.java
@@ -1,9 +1,16 @@
-package by.epam.javabasics.homework02;
+﻿package by.epam.javabasics.homework02;
 
 public class Task35 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 70;
+		int b = 6;
+		
+		double c = (double) a / b;
+		
+		System.out.println("Младшая цифра целой части: " + (int)c % 10);
+		System.out.println("Старшая цифра дробной части: " + (int)(c * 10) % 10);
 
 	}
 
--- by/epam/javabasics/homework02/Task36.java
+++ by/epam/javabasics/homework02/Task36.java
@@ -3,7 +3,28 @@ package by.epam.javabasics.homework02;
 public class Task36 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 3636;
+		int chet = 1;
+		int nechet = 1;
+		int temp = 0;
+		
+		while(a != 0) {
+			temp = a % 10;
+			
+			if(temp % 2 == 0) {
+				chet *= temp;
+			} else {
+				nechet *= temp;
+			}
+			
+			a = a / 10;
+		}
+		
+		double answer = (double) chet / nechet;
+		
+		System.out.println("Answer is: " + answer);
+
 
 	}
 
