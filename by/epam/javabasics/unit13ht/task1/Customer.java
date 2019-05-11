197ec483a8bf1b350ce7495ac5568495f53e1ebf
olegk <olegk@DESKTOP-7844GVK>
2019-05-11 13:07:58 +0300

    new Task

---
+++ by/epam/javabasics/unit13ht/task1/Customer.java
@@ -0,0 +1,60 @@
+package by.epam.javabasics.unit13ht.task1;
+
+public class Customer {
+	
+	private int id;
+	private String name;
+	private String lastName;
+	private String surname;
+	private int cardNumber;
+	private int billingNumber;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	
+	public int getBillingNumber() {
+		return billingNumber;
+	}
+	
+	public void setBillingNumber(int billingNumber) {
+		this.billingNumber = billingNumber;
+	}
+
+}
---
+++ by/epam/javabasics/unit13ht/task1/Student.java
@@ -0,0 +1,88 @@
+package by.epam.javabasics.unit13ht.task1;
+
+public class Student {
+	
+	private int id;
+	private String name;
+	private String lastName;
+	private String surname;
+	private String birthDate;
+	private String address;
+	private String phoneNumber;
+	private String facultet;
+	private int course;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public String getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getFacultet() {
+		return facultet;
+	}
+	
+	public void setFacultet(String facultet) {
+		this.facultet = facultet;
+	}
+	
+	public int getCourse() {
+		return course;
+	}
+	
+	public void setCourse(int course) {
+		this.course = course;
+	}
+	
+	
+}
