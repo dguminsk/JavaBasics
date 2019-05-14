28baffd691816a7da382d70d94b81c19361c91c9
Aleh Kaliada <aleh_kaliada@epam.com>
2019-05-14 13:57:14 +0300

    last changes

---
+++ by/epam/javabasics/finalTask/entity/Library.java
@@ -0,0 +1,44 @@
+package by.epam.javabasics.finalTask.entity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Library {
+	
+	private List<Book> library = new ArrayList<Book>();
+	private String filePath;
+	
+	
+	public void add(Book book) {
+		library.add(book);
+	}
+	
+	public Library() {
+		
+	}
+	
+	public Library(String path) throws FileNotFoundException {
+		filePath = path;
+		Scanner dataFile = new Scanner(new File(filePath));
+		String line;
+		while(dataFile.hasNextLine()) {
+			line = dataFile.nextLine();
+			String[] lineInfo = line.split(";");
+			Book book = new Book(lineInfo[0],lineInfo[1],Integer.parseInt(lineInfo[2]),lineInfo[3]);
+			add(book);
+		}	
+		dataFile.close();
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public List<Book> getLibrary() {
+		return library;
+	}
+
+}
